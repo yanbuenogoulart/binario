8b2f15cea16a6a4b71f14c3a9d48c8763eb86c86
yan <yan_goulart@estudante.sesisenai.org.br>
2024-08-02 11:31:51 -0300

    Versao final

---
+++ script.js
@@ -0,0 +1,16 @@
+function Calcular() {
+    let num = document.getElementById("input").value 
+    let res = document.getElementById("res")
+    let array = []
+    let quociente = num
+    if (num <= 0) {
+        return
+    }
+    while (quociente !== 0) {
+        let resto = Math.floor(quociente % 2)
+        array.push(resto)
+        quociente = Math.floor(quociente / 2)
+    }
+    array.reverse()
+    res.innerHTML = "O número " + num + " em binário é: " + array.join("")
+}
